c3223556320bf83edfda662acf7761bf4d3929e7
Kevin <identityofsine@gmail.com>
2023-08-04 20:13:25 -0400

    Initial

---
+++ wps-client-secret.php
@@ -0,0 +1,12 @@
+/*
+ * Plugin Name:       Stripe Client Secret
+ * Description:       Grabs and Returns the Client Secret based on the users transaction.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Kevin Erdogan
+ * Author URI:        https://identityofsine.github.io/
+ * License:           GPL v2 or later
+ * Text Domain:       stripe-client-secret
+ * Domain Path:       /ih-api
+ */
