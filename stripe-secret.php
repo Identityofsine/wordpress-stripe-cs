6442b372a52b37d3a308ff8d79bbfa3363ddb414
Identityofsine <67929513+Identityofsine@users.noreply.github.com>
2023-08-07 12:24:33 -0400

    added comments

--- stripe-secret.php
+++ stripe-secret.php
@@ -7,3 +7,33 @@ function StripePost() {
 	
 
 }
+
+
+/** 
+  * @summary This function takes in a JSON Object from the POST Request and verifies that the request is valid. The rules for this are in the obsidean file but as a dropoff:
+		*data : {
+		* id:1425,
+		* items:[ 
+		* 	//The expect item object would contain an object id(that corresponds to WooCommerce/Database) and the quantity of the product.
+		* 	
+		* 	{
+		* 		id:24,
+		* 		quantity:1,
+		* 	},
+		* 	{
+		* 		id:14,
+		* 		quantity:2,
+		* 	},
+		* 	{
+		* 		id:54,
+		* 		quantity:1,
+		* 	},
+		* ],
+		* discount_code:'AZJAX',
+		* shipping_method:1,
+		* currency:'USD'
+		*}
+ */
+function VerifyRequest($request) {
+	
+}
